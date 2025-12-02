e6618707d4941c6f2ab6a62acd562bed787b1988
Avi <95044844+Avioti@users.noreply.github.com>
2025-12-02 12:53:40 -0500

    Exercises — NorthwindTraders Pt. 3

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -2,88 +2,25 @@ package com.pluralsight;
 
 import com.mysql.cj.Query;
 
+import java.net.ConnectException;
 import java.sql.*;
 import java.util.Scanner;
 
 public class App {
     static final Scanner scanner = new Scanner(System.in);
+    private static Connection connection = null;
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/northwind";
     public static void main(String[] args) {
-
-
-        String url = "jdbc:mysql://127.0.0.1:3306/northwind";
         String user = args[0];
         String password = args[1];
 
+            loadConnection(args[0],args[1]);
+            run();
 
 
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet results = null;
-
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-
-            while (true) {
-                menu();
-                String query = queryChoice();
-
-                if (query.isBlank()) {
-                    break;
-                }
-
-                statement = connection.prepareStatement(query);
-                results = statement.executeQuery();
 
 
-                if (query.equals(queryProducts())) {
 
-                    while (results.next()) {
-                        System.out.println("-------------------------");
-                        System.out.println("Product ID: " + results.getString("ProductID"));
-                        System.out.println("Product Name: " + results.getString("ProductName"));
-                        System.out.println("Unit Price: " + results.getString("UnitPrice"));
-                        System.out.println("Units In Stock: " + results.getString("UnitsInStock"));
-                        System.out.println("-------------------------");
-                    }
-                } else if (query.equals(queryCustomers())) {
-
-                    while (results.next()) {
-                        System.out.println("-------------------------");
-                        System.out.println("Contact Name: " + results.getString("ContactName"));
-                        System.out.println("Company Name: " + results.getString("CompanyName"));
-                        System.out.println("City: " + results.getString("City"));
-                        System.out.println("Country: " + results.getString("Country"));
-                        System.out.println("Phone: " + results.getString("Phone"));
-                        System.out.println("-------------------------");
-                    }
-                }
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (results != null) {
-                try {
-                    results.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
 
@@ -102,35 +39,92 @@ public class App {
         System.out.print(prompt + ": ");
         return scanner.nextLine();
     }
+    private static int getUserIntInput(String prompt){
+        System.out.print(prompt);
+        String input = scanner.nextLine();
+            return Integer.parseInt(input);
 
-  private static String queryProducts(){
-        return "SELECT * FROM Products";
     }
 
-    private static String queryCustomers(){
-        return "SELECT * FROM Customers ORDER BY Country";
+  private static void queryProducts(){
+       String query = "SELECT * FROM Products";
+
+      try (PreparedStatement statement = connection.prepareStatement(query);
+           ResultSet results = statement.executeQuery()) {
+
+
+
+              while (results.next()) {
+                  System.out.println("-------------------------");
+                  System.out.println("Product ID: " + results.getString("ProductID"));
+                  System.out.println("Product Name: " + results.getString("ProductName"));
+                  System.out.println("Unit Price: " + results.getString("UnitPrice"));
+                  System.out.println("Units In Stock: " + results.getString("UnitsInStock"));
+                  System.out.println("-------------------------");
+              }
+
+
+
+  } catch (SQLException e) {
+        e.printStackTrace();
+    }
     }
 
-    private static String queryChoice(){
-        int choice = Integer.parseInt(getUserInput("Select an option"));
-        String query = "";
-        switch (choice){
-            case 1:
-                query = queryProducts();
-                break;
-            case 2:
-                query = queryCustomers();
-                break;
-            case 0:
-                exit();
-                break;
-            default:
-                System.out.println("Invalid option");
+    private static void queryCustomers(){
+
+        String query = "SELECT * FROM Customers ORDER BY Country";
+
+        try (PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet results = statement.executeQuery()) {
+
+
+                while (results.next()) {
+                    System.out.println("-------------------------");
+                    System.out.println("Contact Name: " + results.getString("ContactName"));
+                    System.out.println("Company Name: " + results.getString("CompanyName"));
+                    System.out.println("City: " + results.getString("City"));
+                    System.out.println("Country: " + results.getString("Country"));
+                    System.out.println("Phone: " + results.getString("Phone"));
+                    System.out.println("-------------------------");
+                }
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void run(){
+        while (true) {
+                menu();
+            switch (getUserIntInput("Select an option: ")) {
+                case 1:
+                   queryProducts();
+                    break;
+                case 2:
+                     queryCustomers();
+                    break;
+                case 3:
+                    //queryCategories();
+                    break;
+                case 0:
+                    exit();
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
         }
 
-        return query;
     }
-
+    public static void loadConnection( String username, String password) {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println("Error when loading connection; Restart app to try again.");
+            e.printStackTrace();
+        }
+    }
 
 
     private static void exit(){
